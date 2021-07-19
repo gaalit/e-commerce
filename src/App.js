2084739f161f92f4fe8402aa7270b7f9e59fcab9
Galit Gerasimov <galit.gerasimov3@gmail.com>
2021-07-19 17:00:15 -0400

    finalized App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,103 @@
-import React from "react";
-import Products from "./components/Products/Products";
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Navbar, Products, Cart, Checkout } from "./components";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
+  // fetching products
+  const [products, setProducts] = useState([]);
+
+  // state for adding items into the cart
+  const [cart, setCart] = useState({});
+
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  // fetching products at initial load
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  //fetching cart from api (cart items are found inside the object under the line_items key and the subtotal is found in the subtotal key)
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart);
+  };
+
+  // function that will add items to our cart
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
+
+  // functiion that updates users cart if they are adding or delting quantities
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const response = await commerce.cart.update(productId, { quantity });
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const response = await commerce.cart.remove(productId);
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(
+        checkoutTokenId,
+        newOrder
+      );
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+  console.log(cart);
   return (
-    <div>
-      E-<Products></Products>
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleAddToCart={handleAddToCart}
+              handleEmptyCart={handleEmptyCart}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout
+              cart={cart}
+              order={order}
+              onCaptureCheckout={handleCaptureCheckout}
+              error={errorMessage}
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
